a16ec878b42664c84fad0decd3fcdf17fd6ca998
Roman Hochuli <roman.hochuli@nexellent.ch>
2015-06-29 20:21:17 +0200

    added note

--- vmware_fusion.sh
+++ vmware_fusion.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# watchout: http://angryfifer.blogspot.ch/2012/05/hgfs-in-opensuse-121-vmware-guest.html
+#
 
 zypper addrepo --no-gpgcheck http://download.opensuse.org/repositories/Virtualization:VMware/openSUSE_13.2/Virtualization:VMware.repo
 zypper refresh
